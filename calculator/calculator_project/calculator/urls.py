5b0810771b13582d9ee26db5de30e3c6d8a0a857
Komalpreet Kaur <komalpreet9113@gmail.com>
2025-02-19 21:55:19 +0530

    Update urls.py

--- calculator/calculator_project/calculator/urls.py
+++ calculator/calculator_project/calculator/urls.py
@@ -1,6 +1,13 @@
+# from django.urls import path
+# from .views import calculator  # Import your view
+
+# urlpatterns = [
+#     path("calculate/", calculator, name="calculator"),  # Correct endpoint
+# ]
 from django.urls import path
-from .views import calculator  # Import your view
+from .views import calculator ,clear_history # Import your view
 
 urlpatterns = [
     path("calculate/", calculator, name="calculator"),  # Correct endpoint
+    path('clear-history/', clear_history, name='clear_history'),
 ]
